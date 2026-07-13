953ae153e715f5d56450fc7f3d3ecde69c696763
hharshitarajoria <hharshitarajoria@gmail.com>
2026-07-13 12:09:07 +0530

    Time: 3 ms (71.03%), Space: 18.4 MB (56.74%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,40 +1,44 @@
 class Solution {
 public:
-    bool dfsCheck(int node,  vector<int> &vis, vector<int> &pathvis,vector<int> adj[],vector<int> &ans){
-        vis[node]=1;
-        pathvis[node]=1;
-
-        for(auto it: adj[node]){
-            if(!vis[it]){
-                if(dfsCheck(it,vis,pathvis,adj,ans) == true) return true;
+    vector<int> CanCompleteCourse(int V, vector<int> adjprerequisites[], vector<int> &inDegree,queue<int> &q){
+        vector<int> courses;
+        int count=0;
+        for(int i=0;i<V ;i++){
+            if(inDegree[i]==0){
+                q.push(i);
             }
-            else if( pathvis[it]) return true;
         }
-        pathvis[node]=0;
-        ans.push_back(node);
-        return false;
+        while(!q.empty()){
+            int node= q.front();
+            q.pop();
+            courses.push_back(node);
+            count++;
+            for( auto it : adjprerequisites[node]){
+                inDegree[it]--;
+                if(inDegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        if(count==V) return courses;
+        else return {};
     }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> adj[numCourses];
-        for (auto it : prerequisites) {
+        int V= numCourses;
+        queue<int> q;
+        vector<int> inDegree(V,0);
 
-            int a = it[0];
-            int b = it[1];
-
-            adj[b].push_back(a);
-        }
-        vector<int> vis(numCourses,0);
-        vector<int> pathvis(numCourses,0);
-        vector<int> ans;
-        
-        for( int i=0;i<numCourses;i++){
-            if(!vis[i]){
-               if(dfsCheck(i,vis,pathvis,adj,ans)==true){
-                  return {};
-               }
+        vector<int> adjprerequisites[V];
+            for(auto it:prerequisites){
+                int a= it[0];
+                int b= it[1];
+                adjprerequisites[b].push_back(a);
+            }
+        for( int i=0;i<V;i++){
+            for(auto it:adjprerequisites[i]){
+                inDegree[it]++;
             }
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        return CanCompleteCourse(V,adjprerequisites,inDegree,q);  
     }
 };
