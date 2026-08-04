c5c911fd3de4e93a98313154f78325c0fa916b84
hharshitarajoria <hharshitarajoria@gmail.com>
2026-08-04 16:12:59 +0530

    Time: 87 ms (16.62%), Space: 18.7 MB (68.96%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -4,8 +4,12 @@ public:
         int m=  matrix.size();
         int n=  matrix[0].size();
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==target) return true;
+            int low=0, high=n-1;
+            while(low<=high){
+                int mid= (low+high)/2;
+                if(matrix[i][mid]==target) return true;
+                if(matrix[i][mid] > target) high=mid-1;
+                else low=mid+1;
             }
          }
          return false;
