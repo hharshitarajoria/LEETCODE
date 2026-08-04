b89f83228e97c43c051d3019781d112662896c7b
hharshitarajoria <hharshitarajoria@gmail.com>
2026-08-04 17:35:06 +0530

    Time: 0 ms (100%), Space: 50.2 MB (58.42%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
+    int maxElem(vector<vector<int>>& mat, int m, int n,int row){
+        int maxInd=-1;
+        int maxVal=-1;
+        for(int i=0; i<n ;i++){
+            if(mat[row][i] > maxVal){
+                maxVal= mat[row][i];
+                maxInd= i;
+            }
+        }
+        return maxInd;
+    }
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int m=mat.size();
-        int delRow[]={0,-1,1,0};
-        int delCol[]={1,0,0,-1};
         int n= mat[0].size();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                bool peak=true;
-                for(int k=0;k<4;k++){
-                    int nrow= i+delRow[k];
-                    int ncol= j+delCol[k];
-                    if(nrow>=0 && ncol>=0 
-                          && nrow<m &&ncol<n){
-                            if( mat[nrow][ncol]>= mat[i][j] ){
-                               peak=false;
-                               break;
-                        }
-                    }
-                }
-                if(peak) return {i,j};
+        int low=0, high=m-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int col= maxElem(mat,m,n,mid);
+            int top = mid-1>=0 ? mat[mid-1][col] : -1; //in bound? yes :no
+            int down = mid+1 <m ? mat[mid+1][col] : -1; //in bound? yes :no
+            if(mat[mid][col]> top && mat[mid][col]>down){
+                return{mid,col};
             }
+            else if(mat[mid][col] < down) low=mid+1;
+            else high=mid-1;
         }
         return {-1,-1};
     }
