889e80af9344d954d4019fc04a99d4806c20f67e
hharshitarajoria <hharshitarajoria@gmail.com>
2026-08-08 22:29:17 +0530

    Time: 36 ms (92.31%), Space: 93.9 MB (69.3%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -8,16 +8,14 @@ public:
                  return a[1] < b[1];
              });
         
-        int cnt = 0;
+        int cnt = 1;
         int end = intervals[0][1];
         for(int i=1;i<n;i++){
-            if(intervals[i][0] < end){
+            if(intervals[i][0] >= end){
                 cnt++;
-            }
-            else{
-                end= intervals[i][1];;
+                end= intervals[i][1];
             }
         }
-        return cnt;
+        return n-cnt;
     }
 };
